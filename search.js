0b824e9370d3fc19d3121c8180971cc06bbc6776
smy1308 <smy1308@gmail.com>
2023-10-22 22:03:35 +0900

    검색기능 완료

---
+++ search.js
@@ -0,0 +1,23 @@
+// let Input = document.querySelector("input");
+// let searchId = Input.value;
+
+// let movieCards = document.querySelectorAll(".movie-card");
+
+// movieCards.forEach((card) => {
+//   let title = card.querySelector(".title").textContent.toLowerCase();
+
+//   if (title.includes(searchId)) {
+//     card.style.display = "flex";
+//   } else {
+//     card.style.display + "none";
+//   }
+// });
+
+// a.forEach((b) => {
+//   console.log(b);
+//   if (b.title.toLowerCase().includes(searchId.toLowerCase())) {
+//     b.movie-card.style.display = "flex";
+//   } else {
+//     b.movie-card.style.display = "none";
+//   }
+// });
